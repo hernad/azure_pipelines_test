79dffa2dce4b46e72d70b165f2db95d4416747e9
Ernad Husremovic <hernad@bring.out.ba>
2019-01-11 15:13:01 +0100

    upload to bintray

---
+++ upload_bintray.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# https://github.com/$BINTRAY_OWNER/greenbox/blob/apps_modular/upload_app.sh
+
+
+BINTRAY_API_KEY=${BINTRAY_API_KEY:-`cat bintray_api_key`}
+
+BINTRAY_OWNER=hernad
+BINTRAY_REPOS=harbour
+BINTRAY_MODULE=$1
+BINTRAY_MODULE_VER=$2
+
+pacman --noconfirm -S curl zip unzip
+pacman --noconfirm -S --needed mingw-w64-$MINGW_ARCH-postgresql mingw-w64-$MINGW_ARCH-icu mingw-w64-$MINGW_ARCH-curl mingw-w64-$MINGW_ARCH-openssl
+mkdir artifacts
+echo "test" > artifacts/test.txt
+
+FILE=${BINTRAY_MODULE}_${BINTRAY_MODULE_VER}.zip
+
+cd artifacts && zip -r -v $FILE .
+
+ls -lh $FILE
+
+echo uploading $FILE to bintray ...
+
+curl -s -T $FILE \
+      -u $BINTRAY_OWNER:$BINTRAY_API_KEY \
+      --header "X-Bintray-Override: 1" \
+     https://api.bintray.com/content/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_MODULE/$BINTRAY_MODULE_VER/$FILE
+
+curl -s -u $BINTRAY_OWNER:$BINTRAY_API_KEY \
+   -X POST https://api.bintray.com/content/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_MODULE/$BINTRAY_MODULE_VER/publish
