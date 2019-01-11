2d01855c34fa902ad41ba456d3c426be68ac93cc
Ernad Husremovic <hernad@bring.out.ba>
2019-01-11 15:26:52 +0100

    upload_bintray.sh

--- upload_bintray.sh
+++ upload_bintray.sh
@@ -7,15 +7,15 @@ BINTRAY_API_KEY=${BINTRAY_API_KEY:-`cat bintray_api_key`}
 
 BINTRAY_OWNER=hernad
 BINTRAY_REPOS=harbour
-BINTRAY_MODULE=$1
-BINTRAY_MODULE_VER=$2
+BINTRAY_PACKAGE=$1
+BINTRAY_PACKAGE_VER=$2
 
 pacman --noconfirm -S curl zip unzip
 pacman --noconfirm -S --needed mingw-w64-$MINGW_ARCH-postgresql mingw-w64-$MINGW_ARCH-icu mingw-w64-$MINGW_ARCH-curl mingw-w64-$MINGW_ARCH-openssl
 mkdir artifacts
 echo "test" > artifacts/test.txt
 
-FILE=${BINTRAY_MODULE}_${BINTRAY_MODULE_VER}.zip
+FILE=${BINTRAY_PACKAGE}_${$BINTRAY_PACKAGE_VER}.zip
 
 cd artifacts && zip -r -v $FILE .
 
@@ -26,7 +26,7 @@ echo uploading $FILE to bintray ...
 curl -s -T $FILE \
       -u $BINTRAY_OWNER:$BINTRAY_API_KEY \
       --header "X-Bintray-Override: 1" \
-     https://api.bintray.com/content/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_MODULE/$BINTRAY_MODULE_VER/$FILE
+     https://api.bintray.com/content/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_PACKAGE/$BINTRAY_PACKAGE_VER/$FILE
 
 curl -s -u $BINTRAY_OWNER:$BINTRAY_API_KEY \
-   -X POST https://api.bintray.com/content/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_MODULE/$BINTRAY_MODULE_VER/publish
+   -X POST https://api.bintray.com/content/$BINTRAY_OWNER/$BINTRAY_REPOS/$BINTRAY_PACKAGE/$BINTRAY_PACKAGE_VER/publish
